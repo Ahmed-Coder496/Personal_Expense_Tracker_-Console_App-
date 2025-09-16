a6c2bd1afeeb8f736babc1d61c1cecf887b464af
Ahmed_Zai <zai015693@gmail.com>
2025-09-16 16:36:51 +0000

    ...

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const dt = new Date()
 // JSON file creation and initialize by []
 
 function createJsonFile() {
-    return fs.writeFileSync(filePath, "[]");
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]");
+  }
 }
-createJsonFile()
+createJsonFile();
 
 // This function reads the JSON file previous expenses
 
@@ -29,10 +31,18 @@ function writeExpenses(expenses) {
 
 // This function reads previous expenses and push the new expenses with ID
 
-function addExpenses(amount, cetegory, subCetegory, description, date) {
+function addExpenses(amount, category, subcategory, description) {
     const expenses = readExpenses()
-    const newExpenses = { id: expenses.length + 1, amount, cetegory, subCetegory, description, date }
-    expenses.push(newExpenses)
+    const newId = expenses.length + 1; 
+    const expense = {
+        id: newId,
+        amount: amount,
+        category: category,
+        subcategory: subcategory,
+        description: description,
+        date: new Date().toLocaleTimeString("en-PK")  // sahi function
+    }
+    expenses.push(expense)
     writeExpenses(expenses)
 
 }
@@ -41,12 +51,12 @@ function addExpenses(amount, cetegory, subCetegory, description, date) {
 
 function viewExpenses() {
     const allExpenses = readExpenses()
-    console.log(`All Expenses:`, allExpenses);
+    console.log(`\n All Expenses:`, allExpenses);
 }
 
-addExpenses("199$", "Food acs", "Resturent", "Mc,Donals Lunch", (dt.toLocaleDateString("en-PK")))
-addExpenses("270$", "travel", "Flight", "Flight of UK", (dt.toLocaleDateString("en-PK")))
-addExpenses("10$", "bills", "Internet Recharge", "Zong ka internet recharge karwana hai", (dt.toLocaleDateString("en-PK")))
+addExpenses(199, "Food acs", "Resturent", "Mc,Donals Lunch", (dt.toLocaleDateString("en-PK")))
+addExpenses(270, "travel", "Flight", "Flight of UK", (dt.toLocaleDateString("en-PK")))
+addExpenses(10, "bills", "Internet Recharge", "Zong ka internet recharge karwana hai", (dt.toLocaleDateString("en-PK")))
 viewExpenses()
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Updation of Expenses ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -63,16 +73,18 @@ function updateExpense(id, updatedData) {
     }
     expenses[index] = { ...expenses[index], ...updatedData };
     writeExpenses(expenses);
-    console.log("Your Expense is Updated:", expenses[index]);
+    console.log("\n Updated Expenses:");
+    console.log(" \n Your Expense is Updated:", expenses[index]);
+    
 }
 
-// updateExpense(2, {amount:"50$" , cetegory:"travel" , subCetegory:"Texi" , description:"Travel by Texi" , date: (dt.toLocaleDateString("en-PK"))})
+// updateExpense(2, {amount:"50$" , category:"travel" , subcategory:"Texi" , description:"Travel by Texi" , date: (dt.toLocaleDateString("en-PK"))})
 
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++ Filtration of Expenses +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function filterExpenses({ id, amount, cetegory, subCetegory, description, keyword }) {
-    const expenses = readExpenses();
+    let expenses = readExpenses();
 
     // Filtration by id
     if (id) {
@@ -103,54 +115,13 @@ function filterExpenses({ id, amount, cetegory, subCetegory, description, keywor
     if (keyword) {
         expenses = expenses.filter(expense => expense.description.toLowerCase().includes(keyword.toLowerCase()));
     }
-
-    console.log("Your searched Expense:", expenses);
+    console.log("\n Filtered Expenses");
+    
+    console.log("\n Your searched Expense:", expenses);
     return expenses;
 }
 
-
-// filterExpenses({ keyword: "internet" })
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++ Deletion of Expenses by ID +++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// This function deletes the expenses by id
-
-function deleteExpense(id) {
-    const expenses = readExpenses();
-    const index = expenses.findIndex(exp => exp.id === id);
-
-    // In this statement if ID equals to -1 so it's returns is Expense not found!
-    if (index === -1) {
-        console.log("Expense not found!");
-        return;
-    }
-
-    // It deletes the expense
-    const deletedExpense = expenses[index];
-    expenses.splice(index, 1);
-
-    console.log(" Expense deleted. Undo within 5 seconds...");
-
-    // +++++++++++++++++++++++++++++++++++++++++ Undo the deletion action in the timer of 5 sec +++++++++++++++++++++++++++++++++++++++++
-
-    setTimeout(() => {
-
-        const yes = true;
-        const no = false
-
-        if (no) {
-            expenses.splice(index, 0, deletedExpense); // restore
-            writeExpenses(expenses);
-            console.log("Deletion Cancelled. Expense Restored!");
-            viewExpenses()
-        } else {
-            writeExpenses(expenses);
-            console.log("Expense Permanently Deleted. And your current expenses are");
-            viewExpenses()
-        }
-    }, 5000);
-}
-// deleteExpense(1)
+// filterExpenses({ id: 2 })
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Sortation of Expenses ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -182,10 +153,59 @@ function sortExpenses(field = "amount", order = "asc") {
         }
     });
 
-    console.log(`Sorted your expenses by ${field} (${order}):`, sorted);
+    console.log("\n Sorted Expense:");
+    
+    console.log(`\n Sorted your expenses by ${field} (${order}):`, sorted);
     return sorted;
 }
-sortExpenses("id", "desc");
+// sortExpenses("id", "desc");
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++ Deletion of Expenses by ID +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// This function deletes the expenses by id
+
+function deleteExpense(id) {
+    const expenses = readExpenses();
+    const index = expenses.findIndex(exp => exp.id === id);
+
+    // In this statement if ID equals to -1 so it's returns is Expense not found!
+    if (index === -1) {
+        console.log("\n Expense not found!");
+        return;
+    }
+
+    // It deletes the expense
+    const deletedExpense = expenses[index];
+    expenses.splice(index, 1);
+
+    console.log("\n Undo Expense");
+    
+    console.log("\n Expense deleted. Undo within 5 seconds...");
+
+    // +++++++++++++++++++++++++++++++++++++++++ Undo the deletion action in the timer of 5 sec +++++++++++++++++++++++++++++++++++++++++
+
+    setTimeout(() => {
+
+        const yes = true;
+        const no = false
+
+        if (no) {
+            expenses.splice(index, 0, deletedExpense); // restore
+            writeExpenses(expenses);
+            console.log("\n Deletion Cancelled. Expense Restored!");
+            viewExpenses()
+        } else {
+            writeExpenses(expenses);
+            console.log("\n Expense Permanently Deleted. And your current expenses are");
+            viewExpenses()
+        }
+    }, 5000);
+}
+// deleteExpense(1)
+
+const expenses = readExpenses()
+const total = expenses.reduce((sum, exp) => sum + exp.amount, 0);
+console.log("💰 Total Expenses:", total);
 
 
 
